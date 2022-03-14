ec3b4aa6dcbf509f4dfe79f30efe6bedf094e3ec
user <user@DESKTOP-6JBEF5K>
2022-03-14 10:18:23 +0800

    ok

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,13 +4,35 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        /*static void Main(string[] args)
         {
             double meal_cost = 100;
             int tip_percent = 15;
             int tax_percent = 8;
             Console.Write("sum=");
             Console.WriteLine(meal_cost+ tip_percent + tax_percent);
+        }*/
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int inputint = int.Parse(input);
+
+            if(inputint % 2 != 0){
+                Console.WriteLine("Werid");
+            }
+            else if(inputint % 2 == 0 && inputint>=2 && inputint<=5){
+                Console.WriteLine("Not Werid");
+            }
+            else if(inputint % 2 == 0 && inputint >=6 && inputint <=20){
+                Console.WriteLine("Werid");
+
+            }
+            else if(inputint % 2 == 0 && inputint > 20){
+                Console.WriteLine("Not Werid")
+            
+            }
+        
         }
     }
+
 }
